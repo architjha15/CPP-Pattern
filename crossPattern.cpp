8d219273d9acf54887f97a927d7b17ab9640c038
architjha15 <103206051+architjha15@users.noreply.github.com>
2022-10-12 19:34:10 +0530

    Add files via upload

---
+++ crossPattern.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void printPattern(int &n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n; j++)
+        {
+
+            if (j == i || j == (n + 1 - i))
+                cout << "*";
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main()
+{
+    int n = 6;
+    cout << "\n-----Cross Pattern-----\n\n";
+    printPattern(n);
+    return 0;
+}
